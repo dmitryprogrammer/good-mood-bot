b1af1f4c40d472f24f84dfd81e9eddefb2a779fb
dmitryprogrammer <dpishchalka@gmail.com>
2026-03-14 15:32:42 +0300

    trying work with asyncio

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from dotenv import load_dotenv
 from os import getenv
+import asyncio
 
 load_dotenv()
 
-print(getenv("HOST_VAR"))
+async def num():
+    async def time():
+        print("Один")
+        await asyncio.sleep(1)
+        print("Два")
+    
+    for i in range(7):
+        await time()
+
+async def main():
+    await asyncio.gather(num())
+
+asyncio.run(main())
