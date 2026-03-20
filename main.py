7cc41c0d6215b178275df1d7db6db2701c48b977
dmitryprogrammer <dpischalka@gmail.com>
2026-03-21 00:27:38 +0300

    Writed bot start message logic

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from dotenv import load_dotenv
-from os import getenv
+from aiogram import Bot, Dispatcher, types
+import os
+import logging
 import asyncio
 
 load_dotenv()
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-async def num():
-    async def time():
-        print("Один")
-        await asyncio.sleep(1)
-        print("Два")
-    
-    for i in range(3):
-        await time()
+logging.basicConfig(level=logging.info)
+
+bot = Bot(token=BOT_TOKEN)
+dispatcher = Dispatcher()
+
+@dispatcher(commands=['start'])
+async def start_message(message: types.Message):
+    await message.reply("Привет! Я бот хорошего настроения!")
 
 async def main():
-    await asyncio.gather(num())
+    await dispatcher.start_polling(bot)
 
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
